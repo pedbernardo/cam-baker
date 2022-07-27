4e5321afaff249b3d343e851182cb380ec684cd7
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-07-27 03:38:07 -0300

    chore: add project setup

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  ignorePatterns: ['src', 'dist'],
+  rules: {
+  }
+}
