7dcd2ef83f3750aed7a666c0b9cfa0631829ca47
Pedro Bernardo <pedrovbernardo@gmail.com>
2023-02-26 17:35:03 -0300

    fix: change bin files to equivalent package.json bin prop
    avoid commander errors when using linux

---
+++ bin/baker.mjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import { Command } from 'commander'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const pkg = require('../package.json')
+
+const BakerCli = new Command()
+
+BakerCli
+  .version(pkg.version)
+  .description('Camunda Baker - CLI for making Tasklist embedded forms easier')
+  .option('-c, --config', 'specify the config file, defaults to camunda.config.js')
+  .command('dev', 'starts a development server and watchers', { isDefault: true })
+  .alias('serve')
+  .command('build', 'create build files for production')
+  .command('stop', 'stops Camunda Run server')
+  .parse(process.argv)
